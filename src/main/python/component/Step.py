be9688e91bd93df24f24ca442126d2e56a8ac9d9
Chen Guo <chen.guo@nomissolutions.com>
2016-02-20 23:25:06 -0800

    refactored task.

--- src/main/python/component/Step.py
+++ src/main/python/component/Step.py
@@ -5,23 +5,21 @@ class Step:
     def __init__(self, taskParent, stepName, config):
         self.task = taskParent
         self.stepName = stepName
-        self.stepDesc = processConfRequest(config, self.__getPath(".DESC"))
-        self.success = processConfRequest(config, self.__getPath(".NEXT.SUCCESS"))
-        self.nextFail = processConfRequest(config, self.__getPath(".NEXT.FAILURE.NEXT_STEP"))
-        self.maxFailures = int(processConfRequest(config, self.__getPath(".NEXT.FAILURE.MAX_FAILURES")))
-        self.waittime = getWaitTime(processConfRequest(config, self.__getPath(".NEXT.FAILURE.WAITTIME")))
-        self.action = processConfRequest(config, self.__getPath(".CALL.ACTION"))
+        self.stepPath = "{0}.STEPS.{1}".format(self.task.taskPath, self.stepName)
+
+        self.stepDesc = processConfRequest(config, self.stepPath + ".DESC")
+        self.success = processConfRequest(config, self.stepPath + ".NEXT.SUCCESS")
+        self.nextFail = processConfRequest(config, self.stepPath + ".NEXT.FAILURE.NEXT_STEP")
+        self.maxFailures = int(processConfRequest(config, self.stepPath + ".NEXT.FAILURE.MAX_FAILURES"))
+        self.waittime = getWaitTime(processConfRequest(config, self.stepPath + ".NEXT.FAILURE.WAITTIME"))
+        self.action = processConfRequest(config, self.stepPath + ".CALL.ACTION")
         self.failures = 0
         self.cursorFail = []
-        self.thread = processConfRequest(config, self.__getPath(".CALL.THREAD"))
+        self.thread = processConfRequest(config, self.stepPath + ".CALL.THREAD")
         self.cursor = getCursor(
-            processConfRequest(config, self.__getPath(".CALL.CURSOR.RANGE")),
-            processConfRequest(config, self.__getPath(".CALL.CURSOR.DATATYPE")),
-            processConfRequest(config, self.__getPath(".CALL.CURSOR.INCREMENT")),
-            processConfRequest(config, self.__getPath(".CALL.CURSOR.FORMAT")))
-        self.cursorListIndex = processConfRequest(config, self.__getPath(".CALL.CURSOR.RANGE"), True)
-        self.curInherit = processConfRequest(config, self.__getPath(".CALL.CURSOR.INHERIT"))
-
-    def __getPath(self, relativePath):
-        return "WORKFLOW.TASKS.{0}.STEPS.{1}{2}" \
-            .format(str(self.task.taskName), str(self.stepName), str(relativePath))
+            processConfRequest(config, self.stepPath + ".CALL.CURSOR.RANGE"),
+            processConfRequest(config, self.stepPath + ".CALL.CURSOR.DATATYPE"),
+            processConfRequest(config, self.stepPath + ".CALL.CURSOR.INCREMENT"),
+            processConfRequest(config, self.stepPath + ".CALL.CURSOR.FORMAT"))
+        self.cursorListIndex = processConfRequest(config, self.stepPath + ".CALL.CURSOR.RANGE", True)
+        self.curInherit = processConfRequest(config, self.stepPath + ".CALL.CURSOR.INHERIT")
---
+++ src/main/python/component/Task.py
@@ -0,0 +1,17 @@
+from src.main.python.util.Tools import *
+
+
+class Task:
+    def __init__(self, taskName, config):
+        self.taskName = taskName
+        self.taskPath = "WORKFLOW.TASKS.{0}".format(str(self.taskName))
+        self.taskDesc = processConfRequest(config, self.taskPath + ".DESC")
+        self.start = processConfRequest(config, self.taskPath + ".START")
+        self.steps = processConfRequest(config, self.taskPath + ".STEPS")
+        self.nextTask = processConfRequest(config, self.taskPath + ".NEXT.SUCCESS")
+        self.nextFail = processConfRequest(config, self.taskPath + ".NEXT.FAILURE")
+        self.branchesDict = {}  # maps of branchName to branch object
+        self.branchValidDict = {}  # for task validating purposes; map branch name to boolean
+        self.links = {}
+        self.restart = []  # list of failed steps; starting point for re-run
+        self.stepObs = {}  # maps stepname to step object
