376ede4c4c11431bd54f9194142eae61b901d64e
Chen Guo <chen.guo@nomissolutions.com>
2016-02-25 22:45:33 -0800

    Added simple 3 step failure test 2.

--- src/test/python/test_noetl.py
+++ src/test/python/test_noetl.py
@@ -11,8 +11,21 @@ class TestNOETL(TestCase):
         filePath = os.path.join(TEST_RESOURCES, "noetlTest_mostBasic.json")
         main([None, filePath])
 
+    def __sameSetupUp(self, fileName, asserts):
+        filePath = os.path.join(TEST_RESOURCES, fileName)
+        generatedFile = os.path.join(TEST_RESOURCES, "ChenTestGeneratedFile")
+        if os.path.exists(generatedFile):
+            os.remove(generatedFile)
+        main([None, filePath])
+        with open(generatedFile) as f:
+            all = f.readlines()
+            print(os.linesep)
+            print(os.linesep.join(all))
+            asserts(all)
+        os.remove(generatedFile)
+
     """
-    step1: cursor[-1], MaxFailure:1
+    step1: cursor[-1], MaxFailure:1, Inherit: False
       |     \
       |      \F
       |S      \
@@ -23,17 +36,31 @@ class TestNOETL(TestCase):
     exit
     """
 
-    def test_goThroughBasicFailure_1(self):
-        filePath = os.path.join(TEST_RESOURCES, "noetlTest_simple3StepFailure_1.json")
-        generatedFile = os.path.join(TEST_RESOURCES, "ChenTestGeneratedFile")
-        if os.path.exists(generatedFile):
-            os.remove(generatedFile)
-        main([None, filePath])
-        with open(generatedFile) as f:
-            all = f.readlines()
-            print(os.linesep)
-            print(os.linesep.join(all))
-            self.assertEquals(2, len(all))
-            self.assertTrue(all[0].startswith(SupportedTestActionsUtils.getPrefixString("step1", "-1")))
-            self.assertTrue(all[1].startswith(SupportedTestActionsUtils.getPrefixString("step1_recovery", "1")))
-        os.remove(generatedFile)
+    def test_simple3StepFailure_1(self):
+        def asserts(allLines):
+            self.assertEquals(2, len(allLines))
+            self.assertTrue(allLines[0].startswith(SupportedTestActionsUtils.getPrefixString("step1", "-1")))
+            self.assertTrue(allLines[1].startswith(SupportedTestActionsUtils.getPrefixString("step1_recovery", "1")))
+
+        self.__sameSetupUp("noetlTest_simple3StepFailure_1.json", asserts)
+
+        """
+    step1: cursor[-1], MaxFailure:1, Inherit: False
+      |     \
+      |      \F
+      |S      \
+      |     step1_recovery: cursor[1],  MaxFailure:1, Inherit: True
+      |     /
+      |    /SF
+      |   /
+    exit
+    """
+
+    def test_simple3StepFailure_2(self):
+        # This one tests cursor inheritance
+        def asserts(allLines):
+            self.assertEquals(2, len(allLines))
+            self.assertTrue(allLines[0].startswith(SupportedTestActionsUtils.getPrefixString("step1", "-1")))
+            self.assertTrue(allLines[1].startswith(SupportedTestActionsUtils.getPrefixString("step1_recovery", "-1")))
+
+        self.__sameSetupUp("noetlTest_simple3StepFailure_2.json", asserts)
