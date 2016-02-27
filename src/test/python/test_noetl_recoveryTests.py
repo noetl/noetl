81c136accbc1516a9d8c8ba45bc34de5d4ba51ef
Chen Guo <chen.guo@nomissolutions.com>
2016-02-26 16:58:26 -0800

    Renamed test_noetl to test_noetl_recoveryTests.

--- src/test/python/test_noetl_recoveryTests.py
+++ src/test/python/test_noetl_recoveryTests.py
@@ -7,7 +7,7 @@ from src.main.python.noetl import *
 from src.rootPath import TEST_RESOURCES
 
 
-class TestNOETL(TestCase):
+class TestNOETL_RecoveryTests(TestCase):
     def test_goThroughMostBasic(self):
         filePath = os.path.join(TEST_RESOURCES, "noetlTest_mostBasic.json")
         main([None, filePath])
