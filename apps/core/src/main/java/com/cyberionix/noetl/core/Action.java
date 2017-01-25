fe2e0efbe4325f829092b6a1b0685beff794d64f
Alexey Kuksin <akuksin@gmail.com>
2017-01-24 22:28:00 -0800

    create maven init project.

---
+++ apps/core/src/main/java/com/cyberionix/noetl/core/Action.java
@@ -0,0 +1,13 @@
+package com.cyberionix.noetl.core;
+
+import java.util.Properties;
+
+/**
+ * Created by refugee on 1/24/17.
+ */
+public class Action {
+    private String name;
+    private String description;
+    private Properties properties;
+
+}
---
+++ apps/core/src/main/java/com/cyberionix/noetl/core/Workflow.java
@@ -0,0 +1,15 @@
+package com.cyberionix.noetl.core;
+
+import java.util.Properties;
+import java.util.HashMap;
+
+/**
+ * Created by refugee on 1/24/17.
+ */
+public class Workflow {
+private String name;
+private String description;
+private String start;
+private HashMap<String,Action> actions;
+private Properties properties;
+}
