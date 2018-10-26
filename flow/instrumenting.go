56ab7f71aa46de4b6b2c1622e36be61cc32acf61
Sergo007 <yatsyna.sergey@gmail.com>
2018-10-26 16:23:59 +0300

    Refactoring infrastructure flow service

--- flow/instrumenting.go
+++ flow/instrumenting.go
@@ -5,28 +5,72 @@ import (
 	"time"
 
 	"github.com/go-kit/kit/metrics"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
 )
 
-type InstrumentingMiddleware struct {
-	RequestCount   metrics.Counter
-	RequestLatency metrics.Histogram
-	CountResult    metrics.Histogram
-	Next           IFlow
+type instrumentingService struct {
+	requestCount   metrics.Counter
+	requestLatency metrics.Histogram
+	countResult    metrics.Histogram
+	Service        Service
 }
 
-func (mw InstrumentingMiddleware) FlowPut(putRequest FlowPutRequest) (output bool, err error) {
+// NewInstrumentingService returns an instance of an instrumenting Service.
+func NewInstrumentingService(s Service) Service {
+	return &instrumentingService{
+		requestCount: kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+			Namespace: "api",
+			Subsystem: "flow_service",
+			Name:      "request_count",
+			Help:      "Number of requests received.",
+		}, []string{"method", "error"}),
+		requestLatency: kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+			Namespace: "api",
+			Subsystem: "flow_service",
+			Name:      "request_latency_microseconds",
+			Help:      "Total duration of requests in microseconds.",
+		}, []string{"method", "error"}),
+		countResult: kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+			Namespace: "api",
+			Subsystem: "flow_service",
+			Name:      "count_result",
+			Help:      "The result of each count method.",
+		}, []string{}),
+		Service:        s,
+	}
+}
+
+func (mw *instrumentingService) FlowPut(putRequest flowPutRequest) (output bool, err error) {
 	defer func(begin time.Time) {
 		lvs := []string{"method", "FlowPut", "error", fmt.Sprint(err != nil)}
-		mw.RequestCount.With(lvs...).Add(1)
-		mw.RequestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+		mw.requestCount.With(lvs...).Add(1)
+		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
 		if output {
-			mw.CountResult.Observe(1)
+			mw.countResult.Observe(1)
 		} else {
-			mw.CountResult.Observe(0)
+			mw.countResult.Observe(0)
 		}
 
 	}(time.Now())
 
-	output, err = mw.Next.FlowPut(putRequest)
+	output, err = mw.Service.FlowPut(putRequest)
+	return
+}
+
+func (mw *instrumentingService) FlowGet(id string) (config string, err error) {
+	defer func(begin time.Time) {
+		lvs := []string{"method", "FlowPut", "error", fmt.Sprint(err != nil)}
+		mw.requestCount.With(lvs...).Add(1)
+		mw.requestLatency.With(lvs...).Observe(time.Since(begin).Seconds())
+		if err!=nil {
+			mw.countResult.Observe(1)
+		} else {
+			mw.countResult.Observe(0)
+		}
+
+	}(time.Now())
+
+	config, err = mw.Service.FlowGet(id)
 	return
 }
--- flow/transport.go
+++ flow/transport.go
@@ -3,64 +3,125 @@ package flow
 import (
 	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 
 	"github.com/go-kit/kit/endpoint"
+	kitlog "github.com/go-kit/kit/log"
+	kithttp "github.com/go-kit/kit/transport/http"
 )
 
-type FlowPutRequest struct {
+type flowPutRequest struct {
 	Id     string `json:"id"`
 	Config string `json:"config"`
 }
 
-type FlowPutResponse struct {
+type flowPutResponse struct {
 	Success bool   `json:"success"`
 	Err     string `json:"err,omitempty"`
 }
 
-func MakeFlowPutEndpoint(svc IFlow) endpoint.Endpoint {
+func makeFlowPutEndpoint(svc Service) endpoint.Endpoint {
 	return func(_ context.Context, request interface{}) (interface{}, error) {
 		// todo 3) тут мы возвращаем конечную точку достуа а именно принимаем раскодированый json и вызываем метод нашей бизнес логики
-		req := request.(FlowPutRequest)
+		req := request.(flowPutRequest)
 		Success, err := svc.FlowPut(req)
 		if err != nil {
-			return FlowPutResponse{Success, err.Error()}, nil
+			return flowPutResponse{Success, err.Error()}, nil
 		}
-		return FlowPutResponse{Success, ""}, nil
+		return flowPutResponse{Success, ""}, nil
 		// и возвращаем ответ для фронтенда (тут мы возвращаем структуру она преобразуется в json в методе encodeResponse() 34 строчка)
 	}
 }
 
-func EncodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
+func encodeResponse(_ context.Context, w http.ResponseWriter, response interface{}) error {
 	// todo 5) тут мы собираем наш ответ для UI в json формат ответ который вернет функция makeFlowPutEndpoint 21 я строка
 	return json.NewEncoder(w).Encode(response)
 }
 
-func DecodeFlowPutRequest(_ context.Context, r *http.Request) (interface{}, error) {
+func decodeFlowPutRequest(_ context.Context, r *http.Request) (interface{}, error) {
 	// todo 2) тут мы раскодируем тело запроса
-	// и в этом же файле описаны методы и структуры для раскодирования и заворачивание в json (вообщем все для транспорта)
-	//switch r.Method {
-	//case http.MethodGet:
-	//	// Serve the resource.
-	//case http.MethodPost:
-	//	// Create a new record.
-	//case http.MethodPut:
-	//	// Update an existing record.
-	//case http.MethodDelete:
-	//	// Remove the record.
-	//default:
-	//	// Give an error message.
-	//}
-	var request FlowPutRequest
+	var request flowPutRequest
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		return nil, err
 	}
 	return request, nil
 }
 
-// эта структура для другого запроса которого еще нет не обращайте внимание
-//type flowGetResponse struct {
-//	Success bool `json:"success"`
-//	Config string `json:"config,omitempty"`
-//	Err string `json:"err,omitempty"`
-//}
+type flowGetRequest struct {
+	Id string
+}
+
+type flowGetResponse struct {
+	Success bool   `json:"success"`
+	Config  string `json:"config"`
+}
+
+func makeFlowGetEndpoint(svc Service) endpoint.Endpoint {
+	return func(_ context.Context, request interface{}) (interface{}, error) {
+		fmt.Println(request)
+		req := request.(flowGetRequest)
+		Config, err := svc.FlowGet(req.Id)
+		if err != nil {
+			return flowPutResponse{false, err.Error()}, nil
+		}
+		return flowGetResponse{true, Config}, nil
+		// и возвращаем ответ для фронтенда (тут мы возвращаем структуру она преобразуется в json в методе encodeResponse() 34 строчка)
+	}
+}
+
+var errBadRoute = errors.New("bad route")
+func decodeFlowGetRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	vars := mux.Vars(r)
+	id, ok := vars["id"]
+	if !ok {
+		return nil, errBadRoute
+	}
+	return flowGetRequest{Id: id}, nil
+}
+
+
+// MakeHandler returns a handler for the booking service.
+func MakeHandler(s Service, logger kitlog.Logger) http.Handler {
+	opts := []kithttp.ServerOption{
+		kithttp.ServerErrorLogger(logger),
+		kithttp.ServerErrorEncoder(encodeError),
+	}
+
+	flowPutHandler := kithttp.NewServer(
+		makeFlowPutEndpoint(s),
+		decodeFlowPutRequest,
+		encodeResponse,
+		opts...,
+	)
+
+	flowGetHandler := kithttp.NewServer(
+		makeFlowGetEndpoint(s),
+		decodeFlowGetRequest,
+		encodeResponse,
+		opts...,
+	)
+
+	r := mux.NewRouter()
+
+	//r.Handle("/flow/v1/template", bookCargoHandler).Methods("POST")
+	r.Handle("/flow/v1/template", flowPutHandler).Methods("PUT")
+	r.Handle("/flow/v1/template/{id}", flowGetHandler).Methods("GET")
+
+	return r
+}
+// encode errors from business-logic
+func encodeError(_ context.Context, err error, w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	switch err {
+	case ErrInvalidArgument:
+		w.WriteHeader(http.StatusBadRequest)
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"error": err.Error(),
+	})
+}
