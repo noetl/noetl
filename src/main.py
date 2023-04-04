dbf4f7563ee24afc701f555ddc8339a0c26aaf30
Alexey Kuksin <akuksin@gmail.com>
2023-04-04 11:15:43 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,14 @@ import yaml
 import signal
 from loguru import logger
 import concurrent.futures
+from __init__ import async_timer
+import nest_asyncio
 
-with open('filename.yaml') as f:
-    my_dict = yaml.safe_load(f)
+nest_asyncio.apply()
+
+def cancel_tasks():
+    logger.info('Got a SIGINT!')
+    tasks: Set[asyncio.Task] = asyncio.all_tasks()
+    logger.info(f'Cancelling {len(tasks)} tasks.')
+    [task.cancel() for task in tasks]
+    
