829ed8bc3fbce6f08830fb9ae9fa37c4728ad2a5
Alexey Kuksin <akuksin@gmail.com>
2023-04-04 11:40:18 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -13,3 +13,14 @@ def cancel_tasks():
     logger.info(f'Cancelling {len(tasks)} tasks.')
     [task.cancel() for task in tasks]
     
+
+@async_timer()
+async def main():
+    event_loop = asyncio.get_event_loop()
+    event_loop.add_signal_handler(signal.SIGINT, cancel_tasks)
+    jobs = []
+    
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
