38e83b9583fd3724e92ce9aec6d62b473203ff7f
akuksin <4165556+akuksin@users.noreply.github.com>
2023-12-14 00:29:15 -0800

    Rename 'workflow_template' to 'playbook_template'

--- noetl/playbook.py
+++ noetl/playbook.py
@@ -5,10 +5,10 @@ from payload import Payload
 class Playbook(Payload):
     def __init__(self, payload_data, nats_pool: NatsConnectionPool | NatsConfig = None, **kwargs):
         super().__init__(payload_data, nats_pool=nats_pool, **kwargs)
-        self.workflow_template = None
+        self.playbook_template = None
 
     async def transit(self):
-        self.workflow_template = self.yaml_value(path="workflow_base64")
+        self.playbook_template = self.yaml_value(path="workflow_base64")
 
     async def generate_command(self):
         pass
