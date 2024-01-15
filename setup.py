30d9512dd175d510ec7d5588b039b933424374fa
akuksin <4165556+akuksin@users.noreply.github.com>
2024-01-14 22:58:07 -0800

    version 0.1.1

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ class CustomInstallCommand(InstallCommand):
 
 setup(
     name="noetl",
-    version="0.1.0",
+    version="0.1.1",
     author="NoETL Team",
     description="NoETL: A Python package for managing workflows",
     packages=find_packages(),
     install_requires=[
         "asyncio==3.4.3",
         "loguru==0.7.2",
-        "aiofiles==23.2.1",
+        "aiofiles==0.23.2.1",
         "aiohttp==3.9.1",
         "pyyaml==6.0",
         "requests>=2.31.0",
@@ -27,9 +27,9 @@ setup(
         "fastapi==0.103.1",
         "uvicorn==0.23.2",
         "nats-py==2.6.0",
-        "strawberry-graphql==0.211.1"
-        "aioprometheus[aiohttp,binary]==23.3.0"
-
+        "strawberry-graphql==0.211.1",
+        "aioprometheus[aiohttp]==23.3.0",
+        "aioprometheus[binary]==23.3.0"
     ],
     entry_points={
         "console_scripts": [
