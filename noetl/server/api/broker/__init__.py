faafd48c5a57b363958b184c5ba8999d1fb6b2f5
kadyapam <182583029+kadyapam@users.noreply.github.com>
2025-09-09 23:49:01 -0700

    refactor: expose `execute_playbook_via_broker` in broker module

--- noetl/server/api/broker/__init__.py
+++ noetl/server/api/broker/__init__.py
@@ -5,9 +5,11 @@ This file only re-exports public symbols; no endpoints are defined here.
 
 from .endpoint import router, encode_task_for_queue
 from .broker import Broker
+from .execute import execute_playbook_via_broker
 
 __all__ = [
     'router',
     'encode_task_for_queue',
     'Broker',
+    'execute_playbook_via_broker',
 ]
