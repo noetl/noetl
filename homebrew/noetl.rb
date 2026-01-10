9b514df386a09eab6687ff9b1d885649c9bd9d71
Kadyapam <182583029+kadyapam@users.noreply.github.com>
2026-01-10 10:00:03 -0800

    chore: Update Homebrew formula to v2.5.4 with correct SHA256

--- homebrew/noetl.rb
+++ homebrew/noetl.rb
@@ -2,7 +2,7 @@ class Noetl < Formula
   desc "NoETL workflow automation CLI - Execute playbooks locally or orchestrate distributed pipelines"
   homepage "https://noetl.io"
   url "https://github.com/noetl/noetl/archive/refs/tags/v2.5.4.tar.gz"
-  sha256 "ca37a41ed35ef0dd1af7f062dade0440f95029738e472c28d389c3b4f9ccbb74" # Will be filled during release
+  sha256 "6929fe568bf6b501c40b4558067bae4352ccad060f90533cec2b4312d4ad15ff"
   license "MIT"
   head "https://github.com/noetl/noetl.git", branch: "master"
 
